605639022652eab02b78ae26a1329d871e01c2f9
Jesper Josefsson <jesper.josefsson@gmail.com>
2014-02-14 22:28:20 +0100

    Add accessors for cell regions in grid I.e. rows, columns and quadrants

--- lib/sudoku/grid.rb
+++ lib/sudoku/grid.rb
@@ -1,16 +1,24 @@
 module Sudoku
   class Grid
     attr_reader :rows
-    def initialize
-      @rows = Array.new(9, Array.new(9, Cell.new))
+    def initialize(rows = nil)
+      @rows = rows
+      @rows ||= Array.new(9, Array.new(9, Cell.new))
     end
 
     def columns
-      rows.transpose
+      @columns ||= rows.transpose
+    end
+
+    def quadrants
+      @quadrants ||= [0,3,6].repeated_permutation(2).inject([]) do |memo, (row, column)|
+        affected_rows = rows[row..row + 2]
+        memo << affected_rows.inject([]) {|quadrant, row| quadrant + row[column..column+2]}
+      end
     end
 
     def cells
-      rows.inject([]) {|sum, row| sum += row}
+      @cells ||= rows.inject([]) {|sum, row| sum += row}
     end
 
     def to_s
@@ -22,7 +30,7 @@ module Sudoku
     class << self
 
       def parse(string)
-        
+        Sudoku::GridParser.new.parse_string(string)
       end
 
     end
--- spec/grid_spec.rb
+++ spec/grid_spec.rb
@@ -2,6 +2,7 @@ require 'spec_helper'
 
 describe Sudoku::Grid do
   let(:grid) { Sudoku::Grid.new }
+  let(:filled_grid) { Sudoku::Grid.parse(Array.new(9, "123456789").join("\n"))}
   
   describe ".rows" do
     it "returns an array of 9 arrays" do
@@ -21,15 +22,53 @@ describe Sudoku::Grid do
   end
 
   describe ".to_s" do
-    it "returns a pretty string" do
-      string = Array.new(9, ".........").join("\n")
-      grid.to_s.should eq(string)
+    context "when the grid is empty" do
+      it "returns a pretty string" do
+        string = Array.new(9, ".........").join("\n")
+        grid.to_s.should eq(string)
+      end
+    end
+
+    context "when the grid has some actual cells" do
+      it "returns a pretty string" do
+        grid = Sudoku::Grid.new
+        grid.cells.each {|cell| cell.value = 1}
+        output = Array.new(9, Array.new(9, "1").join).join("\n")
+        grid.to_s.should eq(output)
+      end      
+    end
+
+    describe "two-way" do
+      it "returns the same string" do
+        input = Array.new(9, "123456789").join("\n")
+        grid = Sudoku::Grid.parse(input)
+        grid.to_s.should eq(input)
+      end
     end
   end
 
   describe ".cells" do
     it "returns a big array of cells" do
       grid.cells.size.should eq(9*9)
+    end
+  end
+
+  describe ".quadrants" do
+    let(:quadrants) do
+      base = [
+        [1,2,3,1,2,3,1,2,3].map(&:to_s),
+        [4,5,6,4,5,6,4,5,6].map(&:to_s),
+        [7,8,9,7,8,9,7,8,9].map(&:to_s)
+      ]
+      base = base + base + base
+    end
+    it "returns correct quadrants" do
+      quadrant_values = filled_grid.quadrants.map{|quadrant| quadrant.map &:to_s }
+      (quadrant_values - quadrants).should be_empty
+    end
+
+    it "returns the correct number of quadrants" do
+      grid.quadrants.size.should eq(9)
     end
   end
 end
