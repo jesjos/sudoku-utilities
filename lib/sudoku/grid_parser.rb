88c23c5a7e1c4601c185fb79cad0dbeb238eec1c
Jesper Josefsson <jesper.josefsson@gmail.com>
2014-02-14 22:27:42 +0100

    Amend grid parser to return an actual grid

--- lib/sudoku/grid_parser.rb
+++ lib/sudoku/grid_parser.rb
@@ -1,9 +1,5 @@
 module Sudoku
   class GridParser
-    def initialize(string)
-      @string = string
-    end
-
     def parse_line(line)
       line.chars.map do |char|
         Sudoku::Cell.parse(char)
@@ -11,9 +7,10 @@ module Sudoku
     end
 
     def parse_string(string)
-      string.each_line.map do |line|
-        parse_line(line)
-      end      
+      rows = string.each_line.map do |line|
+        parse_line(line.strip)
+      end
+      Sudoku::Grid.new rows
     end
   end
 end
