1f0419d46a62e8231a23c08aa1036690aeec2daf
Jesper Josefsson <jesper.josefsson@gmail.com>
2014-02-19 21:03:10 +0100

    Add region accessors to grid

--- lib/sudoku/grid.rb
+++ lib/sudoku/grid.rb
@@ -3,7 +3,7 @@ module Sudoku
     attr_reader :rows
     def initialize(rows = nil)
       @rows = rows
-      @rows ||= Array.new(9, Array.new(9, Cell.new))
+      @rows ||= Array.new(9) {Array.new(9) { Cell.new } }
     end
 
     def columns
@@ -21,11 +21,41 @@ module Sudoku
       @cells ||= rows.inject([]) {|sum, row| sum += row}
     end
 
+    def empty_cells
+      cells.reject {|cell| !cell.empty?}      
+    end
+
     def to_s
-      rows.map do |row|2
+      rows.map do |row|
         row.map(&:to_s).join
       end.join("\n")
     end
+
+    def clone
+      other_rows = rows.map {|row| clone_row(row)}
+      Grid.new(other_rows)
+    end
+
+    def clone_row(row)
+      row.map {|cell| cell.clone}
+    end
+
+    def regions
+      @regions ||= column_regions + row_regions + quadrant_regions
+    end
+
+    def column_regions
+      @column_regions ||= columns.map {|column| Sudoku::Region.new(column)}
+    end
+
+    def row_regions
+      @row_regions ||= rows.map {|row| Sudoku::Region.new(row)}
+    end
+
+    def quadrant_regions
+      @quadrant_regions ||= quadrants.map {|quadrant| Sudoku::Region.new(quadrant)}
+    end
+
 
     class << self
 
