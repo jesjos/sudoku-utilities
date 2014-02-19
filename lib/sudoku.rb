bc78c75bf898f6e9ffc5c19a3e155e1193d707f0
Jesper Josefsson <jesper.josefsson@gmail.com>
2014-02-19 21:03:19 +0100

    Change require structure

--- lib/sudoku.rb
+++ lib/sudoku.rb
@@ -1,5 +1,10 @@
-require 'sudoku/region'
-require 'sudoku/grid'
-require 'sudoku/grid_parser'
-require 'sudoku/cell'
-require 'sudoku/solver'
+require 'thor'
+require 'forwardable'
+['sudoku/region',
+'sudoku/grid',
+'sudoku/grid_parser',
+'sudoku/cell',
+'sudoku/solver',
+'sudoku/command_line'].each do |file|
+  require File.expand_path(file, File.dirname(__FILE__)) 
+end
