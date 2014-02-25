3c6ed0e54cfae96654b489ed6f15164addacb200
Jesper Josefsson <jesper.josefsson@gmail.com>
2014-02-25 08:36:39 +0100

    Remove unused requires

--- lib/sudoku.rb
+++ lib/sudoku.rb
@@ -1,10 +1,8 @@
 require 'thor'
 require 'forwardable'
 require 'hamster'
-['sudoku/region',
-'sudoku/grid',
+['sudoku/grid',
 'sudoku/grid_parser',
-'sudoku/cell',
 'sudoku/solver',
 'sudoku/command_line'].each do |file|
   require File.expand_path(file, File.dirname(__FILE__)) 
