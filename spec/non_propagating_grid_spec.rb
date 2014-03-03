02d20575d1ddeb4f181adabfc9270d606458bfe1
Jesper Josefsson <jesper.josefsson@gmail.com>
2014-03-03 16:22:59 +0100

    Add naive spec for non-propagating grid

---
+++ spec/non_propagating_grid_spec.rb
@@ -0,0 +1,14 @@
+require 'spec_helper'
+
+describe Sudoku::NonPropagatingGrid do
+  let(:grid) { Sudoku::NonPropagatingGrid.new }
+
+  describe ".set" do
+    it "doesn't propagate" do
+      grid.set("A1", 1)
+      grid.values["A1"].size.should eq(1)
+      grid.values.all? {|key, value| key == "A1" || value.size == 9}.should be_true
+    end
+  end
+
+end
