16692105a3e81c91889571404e7ee7c777d34a12
Jesper Josefsson <jesper.josefsson@gmail.com>
2014-03-04 09:24:07 +0100

    Convenience methods in NonPropagatingGrid - Convert to propagating grid - Access values

--- lib/sudoku/non_propagating_grid.rb
+++ lib/sudoku/non_propagating_grid.rb
@@ -9,6 +9,23 @@ module Sudoku
         @values = values.put(key, Hamster.set(value))
       end
     end
+
+    def values_array
+      sorted_keys.map {|key| value_for_key(key)}
+    end
+
+    def value_for_key(key)
+      possible_values = values[key]
+      if possible_values.size == 1
+        possible_values.first
+      else
+        0
+      end
+    end
+
+    def to_propagating_grid
+      PropagatingGrid.new values_array
+    end
     
   end
 end
