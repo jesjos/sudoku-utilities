3da68acb62fac109aebfde4c7fb82a541537e7d4
Jesper Josefsson <jesper.josefsson@gmail.com>
2014-03-09 22:01:49 +0100

    Add command line methods for all actions

--- lib/sudoku/command_line.rb
+++ lib/sudoku/command_line.rb
@@ -2,27 +2,26 @@ require 'curses'
 module Sudoku
   class SudokuSolver < Thor
     desc "read", "Reads a sudoku and prints it"
-    method_option :file, aliases: "-f", desc: "Read from a file and print", required: false
+    method_option :file, aliases: "-f", desc: "Read from a file and print", required: true
     def read(*args)
       string = read_file(*args)
-      grid = Grid.parse(string)
+      grid = Grid.parse(string, NonPropagatingGrid)
       puts grid.to_s
     end
 
     desc "solve", "Reads a sudoku and solves it"
-    method_option :file, aliases: "-f", desc: "Read from a file and solve", required: false
+    method_option :file, aliases: "-f", desc: "Read from a file and solve", required: true
     def solve(*args)
       if path = options[:file]
         begin
-          file = File.open(path)
-          grid = Grid.parse(file.read)
+          grid = Grid.parse(read_file)
           solver = Solver.new(grid)
           result = solver.solve(&print_method)
           window.clear
           window << "Solving took: #{solver.total_time} seconds\n"
           if result
             window << "Solved sudoku:\n"
-            window << solver.solved_grids.first.to_s
+            window << solver.solution.to_s
             window << "\nFound #{solver.solved_grids.size} solution"
           else
             window << "Could not find an answer"
@@ -36,14 +35,29 @@ module Sudoku
       window.close
     end
 
-    private
-
-    def parse_multiple(file)
-      content = file.read
-      content.split("=\n").map {|grid| Grid.parse(grid)}
+    desc "generate", "Generates a Sudoku"
+    method_option :difficulty, aliases: "-d", desc: "Choose a difficulty from [easy, medium, hard, samuraj]", required: false
+    def generate
+      difficulty = options[:difficulty] || "easy"
+      generator = Generator.new
+      puts "Generating Sudoku ..."
+      sudoku = generator.generate(difficulty)
+      puts "Generated #{difficulty} sudoku", sudoku
     end
 
-    def read_file(*args)
+
+    desc "analyze", "Analyzes Sudoku difficulty"
+    method_option :file, aliases: "-f", desc: "Read from a file and print", required: true
+    def analyze
+      string = read_file
+      sudoku = Grid.parse(string, NonPropagatingGrid)
+      analyzer = DifficultyAnalyzer.new(sudoku)
+      puts "Sudoku difficulty is #{analyzer.difficulty}"
+    end
+
+    private
+
+    def read_file(*)
       path = options[:file]
       file = File.open(path)
       file.read
